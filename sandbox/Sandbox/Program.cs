e62baa3e92747e3991c70aa69d4a90c1bb1a425e
Logano04 <loganmolsen04@gmail.com>
2025-04-26 16:43:22 -0600

    bruh man

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("Please enter your name: ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine($"Your first name is {firstName}.");
+
+        Console.Write("Please enter your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your last name is {lastName}.");
     }
 }
