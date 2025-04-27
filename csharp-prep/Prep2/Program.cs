7ba4bf7c7054c43e367d998be6c401cb85b53fb5
Logano04 <loganmolsen04@gmail.com>
2025-04-26 23:43:18 -0600

    grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage?");
+        int grade = int.Parse(Console.ReadLine());
+        string letterGrade = "F";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+            Console.WriteLine($"You got an {letterGrade}");
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+            Console.WriteLine($"You got a {letterGrade}");
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+            Console.WriteLine($"You got a {letterGrade}");
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+            Console.WriteLine($"You got a {letterGrade}");
+        }
+        else
+        {
+            letterGrade = "F";
+
+            Console.WriteLine($"You got an {letterGrade}");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You failed the class.");
+        }
     }
 }
