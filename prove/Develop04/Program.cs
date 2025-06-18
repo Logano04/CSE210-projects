d3e03ad8cf281a9150ad832eeb0f48775502781c
Logano04 <loganmolsen04@gmail.com>
2025-06-18 16:02:04 -0600

    program works now

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,9 +5,37 @@ class Program
     static BreathingActivity BreathingActivity = new BreathingActivity();
     static ReflectingActivity ReflectingActivity = new ReflectingActivity();
     static ListingActivity ListingActivity = new ListingActivity();
+
+    static bool MenuSelect(int selection)
+    {
+        switch (selection)
+        {
+            case 1:
+                BreathingActivity.run();
+                break;
+            case 2:
+                ReflectingActivity.run();
+                break;
+            case 3:
+                ListingActivity.run();
+                break;
+            case 5:
+                return false;
+        }
+        return true;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
-        ListingActivity.run();
+        bool keepGoing = true;
+        while (keepGoing)
+        {
+            Console.WriteLine("Select Activity:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflecting Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Quit");
+            int userInput = int.Parse(Console.ReadLine());
+            keepGoing = MenuSelect(userInput);
+        }
     }
 }
